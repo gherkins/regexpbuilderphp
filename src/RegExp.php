dfbb8f6d19917b4c8e872a1ee7c8e270256ebab9
Max Girkens <maxgirkens@gmail.com>
2015-02-13 10:13:45 +0100

    types, comments

--- src/RegExp.php
+++ src/RegExp.php
@@ -5,33 +5,71 @@ namespace Gherkins\RegExpBuilderPHP;
 class RegExp
 {
 
-    protected $expr;
-    protected $flags;
+    /**
+     * @var String
+     */
+    protected $_expr;
+
+    /**
+     * @var String
+     */
+    protected $_flags;
 
     public function __construct($expr, $flags)
     {
-        $this->expr  = $expr;
-        $this->flags = $flags;
+        $this->_expr  = $expr;
+        $this->_flags = $flags;
     }
 
+    /**
+     * @return String
+     */
     public function __toString()
     {
-        return $this->expr;
+        return $this->getExpression();
     }
 
+    /**
+     * @return String
+     */
+    public function getExpression()
+    {
+        return $this->_expr;
+    }
+
+    /**
+     * @return String
+     */
+    public function getFlags()
+    {
+        return $this->_flags;
+    }
+
+    /**
+     * check string w/ preg_match
+     *
+     * @param $string
+     * @return bool
+     */
     public function test($string)
     {
         return (bool)preg_match(
-            sprintf("/%s/%s", $this->expr, $this->flags),
+            sprintf("/%s/%s", $this->_expr, $this->_flags),
             $string
         );
     }
 
+    /**
+     * execute preg_match, return matches
+     *
+     * @param $string
+     * @return array
+     */
     public function exec($string)
     {
         $matches = array();
         preg_match(
-            sprintf("/%s/%s", $this->expr, $this->flags),
+            sprintf("/%s/%s", $this->_expr, $this->_flags),
             $string,
             $matches
         );
